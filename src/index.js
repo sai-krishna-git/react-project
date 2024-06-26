924941115d027a6fc495ea8dda6e65d90ac0eeae
Sai krishna <Saikrishna1gddm@gmail.com>
2024-06-26 16:22:17 +0530

    learned about diff wayst to apply css , {} in jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function Greeting() {
+//   return React.createElement(
+//     "h3",
+//     {},
+//     "Element created using React.createComponent"
+//   )
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
+}
+const Book = () => (
+  <article className="book">
+    <Img />
+    <Title />
+    <Author />
+  </article>
+)
+
+const Img = () => (
+  <img
+    src="https://m.media-amazon.com/images/I/511Q7vQB90L.jpg"
+    alt="book img"
+  />
+)
+const Title = () => (
+  <h3> Meow Meow: The Incredible True Story of Baby Patankar</h3>
+)
+const Author = () => {
+  const inlineHeadingStyles = {
+    color: "#617d98",
+    fontSize: "0.75rem",
+    marginTop: "0.5rem",
+  }
+  return <h3 style={inlineHeadingStyles}>Srinath Rao</h3>
+}
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<BookList />)
